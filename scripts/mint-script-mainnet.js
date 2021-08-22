998c70b68c18aba5bc0455eeb987b3b5ceb86058
Sweets Sweetman <sweetmantech@gmail.com>
2021-08-22 14:06:53 +0700

    I update mint-script to use env variables to replace hard-coded address.

--- scripts/mint-script-mainnet.js
+++ scripts/mint-script-mainnet.js
@@ -1,10 +1,13 @@
 const hre = require("hardhat");
+require('dotenv').config();
+
+//TODO: ADD YOUR WALLET & CONTRACT ADDRESS.
+const WALLET_ADDRESS = process.env.WALLET_ADDRESS;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
 async function main() {
   const NFT = await hre.ethers.getContractFactory("MyNFT");
   const URI = "ipfs://QmXAwU9MRbysSnkK5PqogFLx97kW5rRGcMckMtCczoGYNj"
-  const WALLET_ADDRESS = "0x1795BD61F0bfE0a3143A1509D07e28862a6C0a44"
-  const CONTRACT_ADDRESS = "0x02973401B0d003e813A562264939BBE70DBFdC87"
   const contract = NFT.attach(CONTRACT_ADDRESS);
   await contract.mint(WALLET_ADDRESS, URI);
   console.log("NFT minted:", contract);
